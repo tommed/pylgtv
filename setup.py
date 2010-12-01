0641bfbac76d2467e6e093f1e3c2eb7b14b90fd7
Tom Medhurst <tom.medhurst@gmail.com>
2010-12-01 16:15:35 +0000

    setup file added for easy installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pylgtv',
+        version='1.0',
+        description='Python tool for controlling LG TVs and other domestic devices',
+        author='Thomas Medhurst',
+        author_email='tom@tommed.co.uk',
+        url='http://www.tommed.co.uk'
+        packages=['pylgtv','pylgtv.televisions','pylgtv.televisions.lg'],
+        )
