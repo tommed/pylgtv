d65979f76e04b9ae90862b50905c4717f5d5ff9a
Tom Medhurst <tom.medhurst@gmail.com>
2010-12-01 13:37:06 +0000

    simple structure added with a few commands

---
+++ pylgtv/__init__.py
@@ -0,0 +1,45 @@
+import pyserial
+
+class BaseDriver(object):
+        '''
+        This base class should be used for all drivers.
+
+        @type  setid: int
+        @param setid: (default 1) the set id for the equipment (most equipment 
+                has a set id to prevent commands going to multiple devices.
+
+        @type  baudrate: int        
+        @param baudrate: (default 9200) the baudrate of the device.
+
+        @type  devaddr: str
+        @param devaddr: (default /dev/ttyUSB0) the device address.
+
+        @type  timeout: int
+        @param timeout: (default 3) The number of seconds to wait for a response.
+        '''
+        def __init__(self, setid=1, baudrate=9200, devaddr='/dev/ttyUSB0', timeout=3):
+                self.setid = setid
+                self.serial = serial.Serial(devaddr, baudrate=baudrate, timeout=timeout)
+
+        def _write(self, msg, expectresponse=False):
+                '''
+                Write data to TX. If L{expectresponse} is C{True}, 
+                then expect a response too.
+
+                @type  expectresponse: bool
+                @param expectresponse: C{True} if a 
+                        response should be checked for afterwards.  
+                '''
+                self.serial.write(msg.format(self.setid))
+                if expectresponse:
+                        return self._read()
+
+        def _read(self, datalen=10):
+                '''
+                Read data from RX.
+
+                @type  datalen: int
+                @param datalen: The amount of bytes to expect (or timeout)
+                '''
+                return self.serial.read(datalen)
+                
